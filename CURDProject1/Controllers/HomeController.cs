23384851dfc0633f44f95d3e9d35ced508804750
Tushar Mankar <tusharmankar02@gmail.com>
2024-07-27 16:58:53 +0530

    Add project files.

---
+++ CURDProject1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using CURDProject1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CURDProject1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult SavePerson(Person p)
+		{
+			SqlConnection sqlconnection = new SqlConnection();
+			sqlconnection.ConnectionString = "Server=localhost;Database=CRUD_project1;User Id=sa;Password=april@131211;";
+			sqlconnection.Open();
+			SqlCommand cmd = new SqlCommand();
+			cmd = sqlconnection.CreateCommand();
+			SqlDataReader res;
+			cmd.CommandText = "Insert into values(null,' " + p.Name + "','" + p.Mobile + "' ,'" + p.Address + "' ,'" + p.City + "')";
+			res = cmd.ExecuteReader();
+			sqlconnection.Close();
+			return View();
+		}
+		public IActionResult AllPerson()
+		{
+			return View();
+		}
+
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ CURDProject1/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace CURDProject1.Models
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Mobile { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+
+	}
+}
